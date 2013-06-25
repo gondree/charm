1ba43026084000f9b60568c9ce40f71366e46adc
MichaelONeil <michael.oneil92@gmail.com>
2013-06-24 23:35:39 -0700

    Final code before unit testing.

--- charm/schemes/cpor_priv_sw05.py
+++ charm/schemes/cpor_priv_sw05.py
@@ -10,14 +10,14 @@ CPOR with Private Verificability
 """ 
 from charm.toolbox.pairinggroup import PairingGroup, GT 
 from charm.core.math.pairing import hashPair as sha1 
-from charm.core.math.integer import integer, random, randomBits 
+from charm.core.math.integer import integer,randomBits,randomPrime,random
 from charm.core.engine.protocol import * 
 from charm.toolbox.symcrypto import MessageAuthenticator 
 from charm.toolbox.RandSubset import RandSubset 
 from charm.toolbox.POR import PORbase 
-from charm.toolbox.integergroup import *
-from charm.core.crypto.cryptobase import selectPRF, AES, MODE_ECB
-import sys, math, argparse 
+from charm.core.crypto.cryptobase import selectPRF,AES,MODE_ECB
+from charm.toolbox.paddingschemes import PKCS7Padding
+import sys, math, argparse
 
 def int2bytes(v):
     v, r = divmod(v, 256)
@@ -36,17 +36,22 @@ class CPORpriv (PORbase):
     self.block_size = 4096
     self.lambda_size = 80
     self.sector_size = (self.lambda_size - 1) / 8
-    self.g = IntegerGroup()
+    self.prime = randomPrime(self.lambda_size, 1)
+    self.padding = PKCS7Padding(block_size = 16)
     PORbase.__init__(self, common_input)
 
   def set_attributes(self, args):
     """ 
-        Implements :py:func:`POR.PORbase.set_attributes()`
-        """ 
+    Implements :py:func:`POR.PORbase.set_attributes()`
+    """ 
     if hasattr (args, 'mac_length'):
       self.mac_key_len = args.mac_length 
     if hasattr (args, 'enc_length'):
       self.enc_key_len = args.enc_length 
+    if hasattr (args, 'prf_length'):
+      self.prf_key_len = args.prf_length
+    if hasattr (args, 'block_size'):
+      self.block_size = args.block_size
     return None 
 
   def keyGen(self):
@@ -54,15 +59,15 @@ class CPORpriv (PORbase):
     Chooses a random symmetric encryption key, and a random MAC key, and combines them
     to create the secret key. No public key.
 
-        Implements :py:func:`POR.PORbase.keyGen()`
-        """ 
-    self.g.paramgen(self.lambda_size) 
+    Implements :py:func:`POR.PORbase.keyGen()`
+    """ 
     pk, sk = dict(), dict()
+    print("Generating MAC key...")
     k = randomBits(self.mac_key_len) 
-    sk["kmac"] = bytes(int2bytes (k)) 
+    sk["kmac"] = bytes(int2bytes (k))
+    print("Generating Encryption key...") 
     k = randomBits(self.enc_key_len) 
     sk["kenc"] = bytes(int2bytes(k)) 
-    print("Finishes KeyGen")
     return (pk, sk) 
 
   def tag (self, filename, pk, sk):
@@ -77,38 +82,28 @@ class CPORpriv (PORbase):
     
     Implements :py:func:`POR.PORbase.tag()`
     """
-    print("Calculating Num_sectors")
-
     # the number of sectors in a block
     num_sectors = int(self.block_size // self.sector_size)
     if (self.block_size % self.sector_size is not 0):
       num_sectors += 1
 
-    print("Opening File")
 
     f = open(filename, 'rb')
     message = f.read()
     f.close()
 
-    print("Closed file, calculating MPRIME")
     
     #TODO: this is where we would transform via erasure-code in future
     Mprime = len(message) 
 
-    print("CALCULATING NUM_BLOCKS")
-    print(Mprime)
-    print(self.block_size)
-
+    print("Determining the number of blocks in the file...")
     num_blocks = Mprime // self.block_size
     if (Mprime % self.block_size is not 0):
       num_blocks += 1 
 
-    print("CREATION OF M[I][J]")
-    print(int(num_blocks))
     m = [[] for i in range(int(num_blocks))]
 
-    print("OPENING FILE TO BE READ IN AS BLOCKS")
-
+    print("Storing file blocks by sectors...")
     # Opening the file and storing the message.
     with open(filename, "rb") as f:
       block = f.read(self.block_size)
@@ -119,11 +114,10 @@ class CPORpriv (PORbase):
         for j in range(int(num_sectors)):
           jstart = j * self.sector_size 
           jend = jstart + min(self.sector_size - 1, len (sectors) - jstart) 
-          m[i].append(sectors[int(jstart):int(jend)])
+          m[i].append(bytes(sectors[int(jstart):int(jend)]))
         block = f.read(self.block_size)
         i = i + 1
     
-    print("FILE READ IN AS BLOCKS")
     
     #
     # make the tags:
@@ -132,13 +126,15 @@ class CPORpriv (PORbase):
     # For simplicity, we skip this and store these privately in local state
     # to ensure Prover-Verifier interaction is a Sigma protocol
     #
+    print("Generating Pseudo Random Function key...")
     kbits = randomBits(self.prf_key_len) 
     kprf = bytes(int2bytes(kbits)) # a random PRF key
-    PRF = selectPRF(AES, (kprf, MODE_ECB))
-
-    alpha = [self.g.random()]#for i in range(num_sectors)] # a list of random numbers from Zp, |alpha| = num_sectors
-    for i in range(len(alpha)):
-        print(alpha[i])
+    
+    print("Generating Pseudo Random Function...")
+    prf = selectPRF(AES,(kprf, MODE_ECB))
+   
+    print("Generating alphas...")
+    alpha = [int(integer(random(self.prime))) for i in range(num_sectors)] # a list of random numbers from Zp, |alpha| = num_sectors
 
     filestate = {}
     filestate["num_blocks"] = num_blocks
@@ -147,11 +143,12 @@ class CPORpriv (PORbase):
     #
     # generate the sigmas
     #
+    print("Calculating sigmas...")
     sigmas =[] # for each block, a sigma is generated with a function using PRFkey on each block, adding the product of all the alphas, and the sectors
-    for i in range (num_blocks):
-      print(alpha[0])
-      am = [alpha[j] * int(struct.unpack('>q', m[i][j])[0]) for j in range (len(m[i]))]
-      s = PRF(i) + sum(am) 
+    for i in range(num_blocks):
+      am = [alpha[j] * int.from_bytes(m[i][j], byteorder='big') for j in range(len(m[i]))]
+      fkprf = int.from_bytes(prf.encrypt(self.padding.encode((i+1).to_bytes(1, byteorder='big'))), byteorder='big')
+      s = fkprf + sum(am) 
       sigmas.append(s) 
     
     data = {}
@@ -170,17 +167,19 @@ class CPORpriv (PORbase):
     #
     # Need to unpack num_blocks, and the encrypted kprf, and alphas to check MAC in packed tag
     #
+    print("Generating Challenge...")
     g = RandSubset()
-    num_blocks = filestate["tag"][0]
-    kprf = filestate["tag"][1]
-    alpha = filestate["tag"][2]
+    num_blocks = filestate["num_blocks"]
+    kprf = filestate["kprf"]
+    alpha = filestate["alpha"]
     count = 0
-    check_set = g.gen(random.randint (1, num_blocks), (num_blocks - 1))
+    #
+    #TODO: Come up with a way to determine how many random numbers to pick
+    #
+    check_set = g.gen(1, (num_blocks - 1))
     # picks a random amount of blocks to check, making sure the amount of blocks
     # picked are within the num_block range
-    NU =[]
-    for i in check_set:
-      NU = self.g.random(self) # same size as check_set, values are generated as random elements from B which is in Zp
+    NU =[int(integer(random(self.prime))) for i in range(len(check_set))]
 
     Q = dict() # set of group of check_set and their corresponding NU values
 
@@ -189,6 +188,7 @@ class CPORpriv (PORbase):
       Q[check_set[x]] = NU[x] 
 
     challenge = Q 
+    chalData = {}
     chalData["kprf"] = kprf 
     chalData["alpha"] = alpha 
     return(challenge, chalData) 
@@ -202,17 +202,23 @@ class CPORpriv (PORbase):
     these values are sent back to the verifier  
     Implements :py:func:`POR.PORbase.generateProof()`
     """
-
+    print("Generating Proof...")
+    m = data["data"]
+    Q = challenge
+    sigmas = data["sigmas"]
     MU = {}
-    for j in range(len(m[i])):
-      add =[NU[i] * m[i][j] for i in range(Q)]
-      MU[j] = sum(add)
+    for j in range(len(m[0])):
+        add = []
+        for i in Q:
+          add.append(Q[i] * int.from_bytes(m[i][j], byteorder='big'))
+        MU[j] = sum(add)
 
-    final_sigma =[NU[i] * sigma[i] for i in range(Q)]
+    final_sigma =[Q[i] * sigmas[i] for i in Q]
     final_sigma = sum(final_sigma) 
     p = {}
     p["MU"] = MU 
     p["final_sigma"] = final_sigma 
+    p["data"] = m
     return p 
 
   def verifyProof(self, proof, challenge, chalData, pk, sk):
@@ -224,15 +230,22 @@ class CPORpriv (PORbase):
     If these values match the sigma returned from the proover then all is good, otherwise abort.
     Implements :py:func:`POR.PORbase.verifyProof()`
     """ 
-    if MU == null:
+    print("Verifying Proof...")
+    MU = proof["MU"]
+    final_sigma = proof["final_sigma"]
+    kprf = chalData["kprf"]
+    alpha = chalData["alpha"]
+    m = proof["data"]
+    Q = challenge
+    if not MU:
       return False
     else:
-      prf = cryptobase.selectPRF(AES, (chalData["kprf"], MODE_ECB)) 
-      temp1 = [NU[i] * prf(i) for i in range(Q)]
-      temp2 = [chalData["alpha"][j] * proof["MU"][j] for j in range(len(m[i]))]
+      prf = selectPRF(AES,(kprf, MODE_ECB)) 
+      temp1 = [Q[i] * int.from_bytes(prf.encrypt(self.padding.encode((i+1).to_bytes(1, byteorder='big'))), byteorder='big') for i in Q]
+      temp2 = [alpha[j] * MU[j] for j in range(len(m[0]))]
       check = sum(temp1) + sum(temp2) 
 
-      if check == proof["final_sigma"]:
+      if check == final_sigma:
         return True
       else:
         return False 
